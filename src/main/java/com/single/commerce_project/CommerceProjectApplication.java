8c208a0eed4c9d2d65285f65f047593a337dec6a
tlsrhksgh <sin99145@naver.com>
2023-01-25 14:45:14 +0900

    Initial commit

---
+++ src/main/java/com/single/commerce_project/CommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.single.commerce_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/single/commerce_project/CommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.single.commerce_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
